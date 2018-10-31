2b03cf5b93a05f884a1a41a4a0e2886552e1eff2
hardboiled65 <hardboiled65@gmail.com>
2018-10-31 23:50:37 +0900

    Developing parser.

--- parser.py
+++ parser.py
@@ -16,6 +16,7 @@ class Blui:
         self.version = ''
         self._items = {}
         self._blui = None
+        self.__level = 0    # For debug print
 
         self._parse(uifile)
 
@@ -27,13 +28,30 @@ class Blui:
         root = tree.getroot()
 
         self.version = root.attrib['version']
-        for child in root:
-            # self._blui = Blui.Node()
-            # self._blui.attributes
-            for it in root.iter():
-                self._items[it.tag] = {}
-                for attr, val in it.attrib.items():
-                    self._items[it.tag][attr] = val
+        self._parse_child(root, None)
+        '''
+        for top in root:
+            self._blui = Blui.Node()
+            for attr, val in top.attrib.items():
+                self._blui.attributes[attr] = val
+            self._parse_child(top, self._blui)
+        '''
+
+    def _parse_child(self, el, node):
+        self.__level += 1
+        for child in el:
+            print((' ' * (self.__level * 2)) + '- ' + child.tag)
+            new_node = Blui.Node()
+            new_node.parent = node
+            if node is None:
+                self._blui = Blui.Node()
+                self._blui.append(new_node)
+            else:
+                node.append(new_node)
+            for attr, val in child.attrib.items():
+                new_node.attributes[attr] = val
+            self._parse_child(child, new_node)
+        self.__level -= 1
 
     def keys(self):
         return self._items.keys()
